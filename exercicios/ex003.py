ad9b121adf84ae92b29a40dd46f09c2b2aa87307
pedrohentec <pedrohentec@gmail.com>
2022-12-27 17:07:56 -0300

    my third exercise

---
+++ exercicios/ex003.py
@@ -0,0 +1,10 @@
+# Soma entre dois valores
+
+valor1 = input('Digite um valor: ')
+valor2 = input('Digite outro valor: ')
+valor1_float = float(valor1)
+valor2_float = float(valor2)
+soma = valor1_float + valor2_float
+print('----------------------------------------------------------------')
+
+print(f'A soma entre {valor1_float} e {valor2_float} é igual a {soma}! ')
