8c5de5db23d000e90783a0c0d0639d2b601356de
pedrohentec <pedrohentec@gmail.com>
2022-12-27 19:41:13 -0300

    my seventh exercise

---
+++ exercicios/ex007.py
@@ -0,0 +1,7 @@
+# Nota média do aluno
+nota1 = input('Primeira nota do aluno: ')
+nota1_float = float(nota1)
+nota2 = input('Segunda nota do aluno: ')
+nota2_float = float(nota2)
+media = (nota1_float + nota2_float) / 2
+print(f'A média entre {nota1_float:.1f} e {nota2_float:.1f} é igual a {media:.1f}')
