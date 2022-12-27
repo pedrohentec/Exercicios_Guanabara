467bc8c67d8a61cc39d0dc0dee08d80d20c5355c
pedrohentec <pedrohentec@gmail.com>
2022-12-27 19:15:57 -0300

    my fifth exercise

---
+++ exercicios/ex005.py
@@ -0,0 +1,5 @@
+# Número antecessor e sucessor de um número definido pelo usuário
+numero = input('Digite um número: ')
+numero_int = int(numero)
+
+print(f'Analisando o valor {numero_int}, seu antecessor é {numero_int - 1} e seu sucessor é {numero_int + 1}')
