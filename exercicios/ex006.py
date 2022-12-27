f8aec5693eb691ea90f305834212be288a7a8662
pedrohentec <pedrohentec@gmail.com>
2022-12-27 19:30:17 -0300

    my sixth exercise

---
+++ exercicios/ex006.py
@@ -0,0 +1,7 @@
+# DOBRO  TRIPLO RAIZ QUADRADA
+
+numero = input('Digite um número: ')
+numero_int = int(numero)
+print(f'O dobro de {numero} é {numero_int * 2}\n')
+print(f'O triplo de {numero} é {numero_int * 3}\n')
+print(f'A raiz quadrada de {numero} é {numero_int ** (1/2):.2f}\n')
