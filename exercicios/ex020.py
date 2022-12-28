637332b8880e12fcd4047c6e8004bbf73d380d15
pedrohentec <pedrohentec@gmail.com>
2022-12-28 19:47:34 -0300

    my twentieth exercise

---
+++ exercicios/ex020.py
@@ -0,0 +1,11 @@
+# Sorteito de ordem alunos
+from random import shuffle
+
+aluno1 = input('Primeiro aluno: ')
+aluno2 = input('Segundo aluno: ')
+aluno3 = input('Terceiro aluno: ')
+aluno4 = input('Quarto aluno: ')
+alunos = [aluno1, aluno2, aluno3, aluno4]
+shuffle(alunos)
+
+print(f'A ordem de apresentação será\n{alunos}')
