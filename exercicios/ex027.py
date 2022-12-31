510ff7a4789a2780e363d5af9c0b04d0cee080b1
pedrohentec <pedrohentec@gmail.com>
2022-12-30 22:02:57 -0300

    my twenty-seventh exercise

---
+++ exercicios/ex027.py
@@ -0,0 +1,7 @@
+# Primeiro e último nome de uma pessoa
+
+nome = input('Digite seu nome completo: ')
+nome = nome.split()
+
+print(f'Seu primeiro nome é {nome[0]}')
+print(f' Seu último nome é {nome[-1]}')
