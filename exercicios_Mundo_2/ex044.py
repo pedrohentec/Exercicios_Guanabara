10860a8ab4233cd441280b3a04573d35b83f6a59
pedrohentec <pedrohentec@gmail.com>
2023-01-10 21:38:32 -0300

    update

--- exercicios_Mundo_2/ex044.py
+++ exercicios_Mundo_2/ex044.py
@@ -1,25 +1,23 @@
-# Desafio 44
-
 preco = float(input('Qual o preço do produto? R$').replace(',', '.'))
-pagamento = input('Qual será a sua forma de pagamento?\n1-Dinheiro/Cheque\n2-Cartão de Crédito\n')
-
-if pagamento == '1': # Pagamento em Dinheiro/Cheque
-    print('Nessa forma de pagamento você terá um desconto de 10% no valor da compra.')
+pagamento = input('''Qual será a sua forma de pagamento?
+1-à vista no Dinheiro/Cheque
+2-à vista no Cartão de Crédito
+3-2x no Cartão de Crédito
+4-3x ou mais no Cartão de Crédito
+''')
+if pagamento == '1':
     desconto = preco - (preco * 10 / 100)
-    print(f'Você irá pagar R${desconto:.2f} ao final da compra')
-elif pagamento == '2': # Cartão
-    pagamento_cartao = input('Vai ser à vista ou parcelado?\n1-À Vista\n2-Parcelado\n')
-    if pagamento_cartao == '1':
-        print('Nessa forma de pagamento você terá um desconto de 5% no valor da compra.')
-        desconto = preco - (preco * 5 / 100)
-        print(f'Você irá pagar R${desconto:.2f} ao final da compra')
-    else:
-        parcelado_cartao = int(input('Deseja pagar em quantas vezes? '))
-        if parcelado_cartao <= 2:
-            print(f'Você irá pagar R${preco:.2f} ao final da compra')
-        else:
-            print('Nessa forma de pagamento você terá um acréscimo de 20% de juros')
-            juros = preco + (preco * 20 / 100)
-            print(f'Você irá pagar R${juros:.2f} ao final da compra')
+    print(f'Você tem um desconto de 10%, sendo assim irá pagar R${desconto:.2f} ao final da compra')
+elif pagamento == '2':
+    desconto = preco - (preco * 5 / 100)
+    print(f'Você tem um desconto de 5%, sendo assim irá pagar R${desconto:.2f} ao final da compra')
+elif pagamento == '3':
+    preco_parcelas = preco / 2
+    print(f'Você irá pagar R${preco:.2f} em 2 parcelas de R${preco_parcelas:.2f} ao final da compra')
+elif pagamento == '4':
+    juros = preco + (preco * 20 / 100)
+    parcelas = int(input('Quantas parcelas? '))
+    preco_parcelas = preco / parcelas
+    print(f'Você terá um acréscimo 20% de juros, sendo assim irá pagar R${juros:.2f} em {parcelas} parcelas de R${preco_parcelas:.2f} ao final da compra')
 else:
     print('Você não digitou uma opção válida')
