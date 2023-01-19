0fe23682b5095c51f5d96e95246e01a22cb24f4b
pedrohentec <pedrohentec@gmail.com>
2023-01-19 20:21:23 -0300

    tenth of the third world

---
+++ exercicios_Mundo_2/ex046.py
@@ -0,0 +1,8 @@
+# Desafio 46
+from time import sleep
+print('Contagem Regressiva para estouro de fogos de artificios ! ! ! ')
+sleep(1)
+for contagem in range(10, -1, -1):
+    print(contagem)
+    sleep(1)
+print('ATIVANDO FOGOS DE ARTIFICIO 🎆🎆🎆')
