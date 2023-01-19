929f5ac54678983766754d8978c63b32e0a634c7
pedrohentec <pedrohentec@gmail.com>
2023-01-19 20:33:45 -0300

    tenth of the fourth world

---
+++ exercicios_Mundo_2/ex047.py
@@ -0,0 +1,9 @@
+# Desafio 47
+lista_nums = []
+
+for num in range(1, 51):
+    if num % 2 == 0:
+        lista_nums.append(num)
+
+print('Na contagem de 1 a 50, esses são os números pares:')
+print(* lista_nums)
